90c8ded30fef77d8fade1e138ee2d736bb853a7a
Eugene Katrukha <katpyxa@gmail.com>
2026-04-22 14:28:46 +0200

    v.0.9.0

--- src/main/java/bigtrace/BigTrace.java
+++ src/main/java/bigtrace/BigTrace.java
@@ -1,16 +1,28 @@
 package bigtrace;
 
 import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Desktop;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.WindowConstants;
 
@@ -188,9 +200,46 @@ public class BigTrace < T extends RealType< T > & NativeType< T > > implements P
 		{
 			if(Prefs.get( "BigTrace.bUpdateSiteNotice", true ))
 			{
-				IJ.showMessage( "IMPORTANT! UPDATE SITE CHANGES", "For the future releases BigTrace moves to its own update site!\n"
-						+ "This release (0.9.0) is the last release on Ekatrukha update site!\n"
-						+ "Please add https://sites.imagej.net/BigTrace/ to enable future updates!" );
+				JPanel pWarning = new JPanel(new GridBagLayout());
+				
+				String message  = "<html>BigTrace moved to a new, its own update site (listed in FIJI update window)!<br/>";
+				message = message + "This release (0.9.0) is the last release on Ekatrukha update site.</html>";
+				JLabel hyperlink = new JLabel("Please follow new installation intructions");
+				hyperlink.setForeground(Color.BLUE.darker());
+				hyperlink.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+				hyperlink.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) 
+					{
+						try
+						{
+							Desktop.getDesktop().browse(new URI("https://github.com/UU-cellbiology/BigTrace/wiki/How-to-install-plugin"));
+						}
+						catch ( IOException | URISyntaxException exc )
+						{
+							exc.printStackTrace();
+						}
+
+					}
+				});
+				String[] options = {"OK"};
+//				IJ.showMessage( "IMPORTANT! UPDATE SITE CHANGES", "For the future releases BigTrace moves to its own update site!\n"
+//						+ "This release (0.9.0) is the last release on Ekatrukha update site!\n"
+//						+ "Please add https://sites.imagej.net/BigTrace/ to enable future updates!" );
+				
+				GridBagConstraints gbc = new GridBagConstraints();
+				gbc.insets = new Insets(8,0,8,0);
+				gbc.gridx = 0;
+				gbc.gridy = 0;
+				pWarning.add( new JLabel(message), gbc );
+				gbc.insets = new Insets(0,0,0,0);
+				gbc.gridy++;
+				pWarning.add( hyperlink, gbc );
+
+				
+				JOptionPane.showOptionDialog(null, pWarning, "IMPORTANT! UPDATE SITE CHANGES", 
+						JOptionPane.PLAIN_MESSAGE, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+
 			}
 		}
 		
